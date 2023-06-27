32f28a859cf79d540ee040985ea1e047285db254
vuhaianh0907 <vuhaianh0907@gmail.com>
2023-06-27 15:48:10 +0700

    login by Viet

--- src/BodyContent/LoginBar.js
+++ src/BodyContent/LoginBar.js
@@ -5,108 +5,103 @@ import GoogleLogin from 'react-google-login';
 import './LoginBar.css';
 
 export default function LoginBar() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleLogout = () => {
-        setIsLoggedIn(false);
-    };
+  const handleLoginSuccess = (response) => {
+    // Lưu trữ token vào session storage
+    sessionStorage.setItem('token', response.data.token);
+    // Thực hiện các hành động khác sau khi đăng nhập thành công
+    // Chuyển hướng đến trang đăng nhập
+    window.location.href = '/';
+    console.log('Logged in successfully');
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const data = {
-            email: email,
-            password: password
-        };
-
-        axios
-            .post('https://dencelclinic.onrender.com/api/auth/login', data)
-            .then((response) => {
-                console.log(response.data);
-                setIsLoggedIn(true);
-                alert(`Chào mừng ${response.data.fullname}!`); // Hiển thị thông báo chào mừng
-            })
-            .catch((error) => {
-                if (error.response) {
-                    setErrorMessage(error.response.data.message);
-                } else {
-                    setErrorMessage('An error occurred. Please try again later.');
-                }
-            });
-    };
-
-    const handleGoogleLoginSuccess = (response) => {
-        console.log(response);
-    };
-
-    const handleGoogleLoginFailure = (error) => {
-        console.log(error);
-    };
-
-    if (isLoggedIn) {
-        return (
-            <div>
-                {/* Thêm nội dung chào mừng */}
-                <div>Xin chào!</div>
-            </div>
-        );
+  const handleLoginFailure = (error) => {
+    if (error.response) {
+      setErrorMessage(error.response.data.message);
     } else {
-        return (
-            <form className="login-form" onSubmit={handleSubmit}>
-                <h3>Đăng nhập</h3>
-
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        id="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-
-                {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-                <button type="submit">Đăng nhập</button>
-
-                <div className="login-links">
-                    <Link to="/register">Đăng ký tài khoản</Link>
-                    <Link to="/forgot-password">Quên mật khẩu</Link>
-                </div>
-
-                <div className="social">
-                    <GoogleLogin
-                        clientId="YOUR_GOOGLE_CLIENT_ID"
-                        buttonText=" Đăng nhập bằng Google"
-                        onSuccess={handleGoogleLoginSuccess}
-                        onFailure={handleGoogleLoginFailure}
-                        cookiePolicy={'single_host_origin'}
-                    />
-                </div>
-            </form>
-        );
+      setErrorMessage('An error occurred. Please try again later.');
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: email,
+      password: password
+    };
+
+    axios.post('https://dencelclinic.onrender.com/api/auth/login', data)
+      .then(handleLoginSuccess)
+      .catch(handleLoginFailure);
+  };
+
+  const handleGoogleLoginSuccess = (response) => {
+    // Xử lý thành công khi đăng nhập bằng tài khoản Google
+    // Gửi thông tin đăng nhập thành công lên server hoặc thực hiện hành động khác
+    console.log(response);
+  };
+
+  const handleGoogleLoginFailure = (error) => {
+    // Xử lý khi đăng nhập bằng tài khoản Google thất bại
+    console.log(error);
+  };
+
+  return (
+    <form className='login-form' onSubmit={handleSubmit}>
+      <h3>Login Here</h3>
+
+      <div className='form-group'>
+        <label htmlFor='email'></label>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='password'></label>
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+
+      {errorMessage && <p className='error-message'>{errorMessage}</p>}
+
+      <button type='submit'>Log In</button>
+
+      <div className='login-links'>
+        <a href='/register'>Register</a>
+        <a href='/forgot-password'>Forgot Password</a>
+      </div>
+
+      <div className='social'>
+        <GoogleLogin
+          clientId='YOUR_GOOGLE_CLIENT_ID'
+          buttonText='Google'
+          onSuccess={handleGoogleLoginSuccess}
+          onFailure={handleGoogleLoginFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    </form>
+  );
 }
--- src/Home/header.js
+++ src/Home/header.js
@@ -1,51 +1,56 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo-01-01.png';
+import '../App.css';
+import React from 'react';
 
 export default function Navigation() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // Thêm state isLoggedIn và hàm setIsLoggedIn
+  const isLoggedIn = sessionStorage.getItem('token') !== null;
 
-    const handleLogout = () => {
-        setIsLoggedIn(false); // Cập nhật trạng thái đăng nhập
-    };
+  const handleLogout = () => {
+    // Xử lý logout tại đây
+    // Xóa token và các thông tin khác khỏi sessionStorage
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('expirationTime');
+    // Chuyển hướng đến trang đăng nhập
+    window.location.href = '/login';
+  };
 
-    return (
-        <div className="nav">
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list--logo">
-                        <a href="/">
-                            <img src={logo} className="App-logo" alt="logo" />
-                        </a>
-                    </li>
-                    <li className="nav-list--element">
-                        <a href="/Banner">Banner</a>
-                    </li>
-                    <li className="nav-list--element nav-list--element-right">
-                        <a href="/Booking">Booking</a>
-                    </li>
-                    <li className="nav-list--element">
-                        <a href="/Doctor">Doctor</a>
-                    </li>
-                    <li className="nav-list--element">
-                        <a href="/crDoc">crDoc</a>
-                    </li>
-                    <li className="nav-list--element">
-                        <a href="/Worksheet">View Worksheet</a>
-                    </li>
-                </ul>
-
-                <div className="nav-list--prior">
-                    {isLoggedIn ? (
-                        <div>
-                            <div>Xin chào! Bạn đã đăng nhập thành công.</div>
-                            <button onClick={handleLogout}>Logout</button>
-                        </div>
-                    ) : (
-                        <a href="/Login">Login</a>
-                    )}
-                </div>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="nav">
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list--logo">
+            <a href="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </a>
+          </li>
+          <li className="nav-list--element">
+            <a href="/Banner">Banner</a>
+          </li>
+          <li className="nav-list--element nav-list--element-right">
+            <a href="/Booking">Booking</a>
+          </li>
+          <li className="nav-list--element">
+            <a href="/Doctor">Doctor</a>
+          </li>
+          <li className="nav-list--element">
+            <a href="/crDoc">crDoc</a>
+          </li>
+          <li className="nav-list--element">
+            <a href="/Worksheet">View Worksheet</a>
+          </li>
+        </ul>
+        {isLoggedIn ? (
+          <div className="nav-list--prior">
+            <a href="/Logout" onClick={handleLogout}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="nav-list--prior">
+            <a href="/Login">Login</a>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
 }
