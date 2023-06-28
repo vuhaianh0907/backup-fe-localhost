596df217f4257d2db1b055323b7340ee1443e632
LucDQ <lucdq410@gmail.gmailcom>
2023-06-28 13:49:56 +0700

    sapxepfile

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navigation from './Home/header';
-import Banner from './BodyContent/banner.js';
-import Login from './BodyContent/LoginBar';
+import Banner from './BodyContent/banner/banner.js';
+import Login from './BodyContent/accout/login/LoginBar';
 import Doctor from './BodyContent/Doctor';
-import Register from './BodyContent/Register';
-import ForgotPassword from './BodyContent/ForgotPassword';
+import Register from './BodyContent/accout/Register/Register';
+import ForgotPassword from './BodyContent/accout/ForgotPassword/ForgotPassword';
 import Footer from './Footer/footer';
 import DocDetail from './BodyContent/DocDetail';
 import Worksheet from './BodyContent/Worksheet';
-import crDoc from './function/crDoc';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -32,7 +31,6 @@ function App() {
           <Route path='/forgot-password' component={ForgotPassword} />
           <Route path='/' exact component={Banner} />
           <Route path='/Worksheet' component={Worksheet} />
-          <Route path='/crDoc' component={crDoc} />
         </Switch>
         <Footer />
       </div>
