fb751bda4de978f713bb5997921c3f2f7a0400d4
LucDQ <lucdq410@gmail.gmailcom>
2023-07-02 14:15:09 +0700

    Doctor Create Treatment In

--- src/BodyContent/Doctor/CreateTreatementIn/CreateTreatmentIn.js
+++ src/BodyContent/Doctor/CreateTreatementIn/CreateTreatmentIn.js
@@ -59,7 +59,7 @@ function CreateTreatmentIn() {
 
   return (
     <div className="profile-page">
-      <h2>Profile</h2>
+      <h2>Cập nhật tiến trình trị liệu</h2>
       {/* Hiển thị thông tin người dùng */}
       <div className="profile-details">
         <div>
@@ -67,38 +67,43 @@ function CreateTreatmentIn() {
           <input type="text" value="John Doe" readOnly />
         </div>
         <div>
-          <label>ID:</label>
+          <label>CMND/CCCD</label>
           <input type="text" value="123456" readOnly />
         </div>
         <div>
           <label>Gender:</label>
-          <input type="text" value="Male" readOnly />
+          <input type="radio" name="gender" value="Male" checked />
+          <label for="male">Nam</label>
+          <input type="radio" name="gender" value="Female" />
+          <label for="female">Nữ</label>
         </div>
+        <h3>Thông tin điều trị</h3>
         <div>
-          <label>Treatment Date:</label>
+          <label>Ngày khám bệnh </label>
           <input type="text" value="2023-06-28" readOnly />
         </div>
       </div>
       {/* Hiển thị form nhập liệu */}
       <div className="input-form">
-        <h3>Treatment Profile</h3>
+        <h3>Hồ sơ bệnh</h3>
         <div className="treatment-profile">
-          <input type="text" value="Sample Treatment Profile" readOnly />
+          <input type="text" value="Trám răng" readOnly />
         </div>
         <div className="input-boxes">
           {/* Hiển thị ô nhập liệu cho treatmentProgress */}
           {treatmentProgress.map((progress, index) => (
             <div className="input-box" key={index}>
+              <label>Quá trình điều trị:</label>
               <input
                 type="text"
                 value={progress}
                 onChange={(e) => handleProgressChange(index, e.target.value)}
-                placeholder="Treatment Progress"
+                placeholder="Bệnh nhân bị ngố, ánh mắt thất thần, đôi bàn tay run rẩy"
               />
               {/* Nút thêm ô nhập liệu cho treatmentProgress */}
               {index === treatmentProgress.length - 1 && (
                 <button onClick={() => handleAddInputBox('treatmentProgress')}>
-                  Add
+                  Thêm thông tin
                 </button>
               )}
             </div>
@@ -106,30 +111,32 @@ function CreateTreatmentIn() {
           {/* Hiển thị ô nhập liệu cho results */}
           {results.map((result, index) => (
             <div className="input-box" key={index}>
+              <label>Kết quả:</label>
               <input
                 type="text"
                 value={result}
                 onChange={(e) => handleResultChange(index, e.target.value)}
-                placeholder="Result"
+                placeholder="Bệnh nhân bị ngố, ánh mắt thất thần, đôi bàn tay run rẩy"
               />
               {/* Nút thêm ô nhập liệu cho results */}
               {index === results.length - 1 && (
-                <button onClick={() => handleAddInputBox('results')}>Add</button>
+                <button onClick={() => handleAddInputBox('results')}>Thêm thông tin</button>
               )}
             </div>
           ))}
           {/* Hiển thị ô nhập liệu cho advice */}
           {advice.map((adviceText, index) => (
             <div className="input-box" key={index}>
+              <label>Lời khuyên:</label>
               <input
                 type="text"
                 value={adviceText}
                 onChange={(e) => handleAdviceChange(index, e.target.value)}
-                placeholder="Advice"
+                placeholder="Cần giữ gìn sức khỏe răng miệng đánh răng trước và sau khi ăn "
               />
               {/* Nút thêm ô nhập liệu cho advice */}
               {index === advice.length - 1 && (
-                <button onClick={() => handleAddInputBox('advice')}>Add</button>
+                <button onClick={() => handleAddInputBox('advice')}>Thêm thông tin</button>
               )}
             </div>
           ))}
@@ -138,10 +145,10 @@ function CreateTreatmentIn() {
       {/* Hiển thị các nút hoàn thành và tái khám */}
       <div className="action-buttons">
         <button className="complete-button" onClick={handleComplete}>
-          Complete
+          Xong
         </button>
         <button className="reappoint-button" onClick={handleReappoint}>
-          Re-appoint
+          Đặt tái khám
         </button>
       </div>
     </div>
