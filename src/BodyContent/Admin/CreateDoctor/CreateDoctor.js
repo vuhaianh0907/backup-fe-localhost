61bb40c3121b7ec08a7cdf33417a830702b04618
LucDQ <lucdq410@gmail.gmailcom>
2023-07-13 10:33:23 +0700

    scss Createdoctor

--- src/BodyContent/Admin/CreateDoctor/CreateDoctor.js
+++ src/BodyContent/Admin/CreateDoctor/CreateDoctor.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import './CreateDoctor.css';
+import './CreateDoctor.scss';
+import swal from 'sweetalert';
 import axios from 'axios';
 
 function AdminCreateDoctor() {
@@ -18,13 +19,13 @@ function AdminCreateDoctor() {
         qualification: '',
         experience: '',
     });
-    
+
     const [accountInfo, setAccountInfo] = useState({
         password: '',
     });
-    
+
     const [avatar, setAvatar] = useState(null);
-    
+
     const handlePersonalChange = (event) => {
         const { name, value } = event.target;
         setPersonalInfo((prevInfo) => ({
@@ -32,7 +33,7 @@ function AdminCreateDoctor() {
             [name]: value,
         }));
     };
-    
+
     const handleProfessionalChange = (event) => {
         const { name, value } = event.target;
         setProfessionalInfo((prevInfo) => ({
@@ -40,7 +41,7 @@ function AdminCreateDoctor() {
             [name]: value,
         }));
     };
-    
+
     const handleAccountChange = (event) => {
         const { name, value } = event.target;
         setAccountInfo((prevInfo) => ({
@@ -48,12 +49,12 @@ function AdminCreateDoctor() {
             [name]: value,
         }));
     };
-    
+
     const handleImageChange = (event) => {
         const file = event.target.files[0];
         setAvatar(file);
     };
-    
+
     const convertImageToBase64 = (file) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
@@ -62,16 +63,16 @@ function AdminCreateDoctor() {
             reader.onerror = (error) => reject(error);
         });
     };
-    
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
+
         // Chuyển đổi hình ảnh sang base64
         let avatarData = null;
         if (avatar) {
             avatarData = await convertImageToBase64(avatar);
         }
-    
+
         // Xử lý gửi form với dữ liệu đã chuyển đổi
         const formData = {
             personalInfo,
@@ -79,170 +80,210 @@ function AdminCreateDoctor() {
             accountInfo,
             avatar: avatarData,
         };
-    
+
         // Gửi dữ liệu đến server hoặc xử lý theo yêu cầu của bạn
         axios.post('http://localhost:3000/api/admin/createdoctor', formData)
-        .then(response => {
-          alert(response.data);
-          // Xử lý kết quả từ server
-        })
-        .catch(error => {
-          console.error(error);
-          if (error.response && error.response.data && error.response.data.error) {
-            setError(error.response.data.error);
-          } else {
-            setError('Có lỗi xảy ra khi gửi yêu cầu đến server');
-          }
-        });
+            .then(response => {
+                alert(response.data);
+                // Xử lý kết quả từ server
+            })
+            .catch(error => {
+                console.error(error);
+                if (error.response && error.response.data && error.response.data.error) {
+                    setError(error.response.data.error);
+                    swal('Lỗi', error.response.data.error, 'error');
+                } else {
+
+                    swal('Lỗi', 'Có lỗi xảy ra khi gửi yêu cầu đến server', 'error');
+                }
+            });
+
         console.log(formData);
     };
 
     return (
-        <div className="form1">
-            <form onSubmit={handleSubmit} className="form-container">
-                <h2>Hồ sơ bác sĩ</h2>
-                <h3>Thông tin cá nhân</h3>
-                {error !== null && <div className="error">{error}</div>}
-                
+        <div className='form1'>
+            <div id="CreateDoctor" className="form1 mb-3">
+                <form onSubmit={handleSubmit} className="form-container">
+                    <h2 className="mb-3">Hồ sơ bác sĩ</h2>
+                    <h3 className="mb-3">Thông tin cá nhân</h3>
+                    {error !== null && <div className="error">{error}</div>}
 
-                <label htmlFor="fullName">Họ và tên:</label>
-                <input
-                    type="text"
-                    id="fullName"
-                    name="fullName"
-                    value={personalInfo.fullName}
-                    onChange={handlePersonalChange}
-                    placeholder="Nhập tên"
-                />
-
-                <label htmlFor="idCard">CMND/CCCD:</label>
-                <input
-                    type="text"
-                    id="idCard"
-                    name="idCard"
-                    value={personalInfo.idCard}
-                    onChange={handlePersonalChange}
-                    placeholder="Nhập CMND/CCCD"
-                />
-
-                <label htmlFor="gender">Giới tính:</label>
-                <div className="gender-options">
-                    <label>
+                    <div className="mb-3 mt-3">
+                        <label htmlFor="fullName" className="form-label">Họ và tên:</label>
                         <input
-                            type="radio"
-                            name="gender"
-                            value="Nam"
-                            checked={personalInfo.gender === 'Nam'}
+                            type="text"
+                            className="form-control"
+                            id="fullName"
+                            name="fullName"
+                            value={personalInfo.fullName}
                             onChange={handlePersonalChange}
+                            placeholder="Nhập tên"
                         />
-                        Nam
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="gender"
-                            value="Nữ"
-                            checked={personalInfo.gender === 'Nữ'}
-                            onChange={handlePersonalChange}
-                        />
-                        Nữ
-                    </label>
-                </div>
-
-                <label htmlFor="dateOfBirth">Ngày sinh:</label>
-                <input
-                    type="date"
-                    id="dateOfBirth"
-                    name="dateOfBirth"
-                    value={personalInfo.dateOfBirth}
-                    onChange={handlePersonalChange}
-                />
-
-                <label htmlFor="phone">Số điện thoại:</label>
-                <input
-                    type="text"
-                    id="phone"
-                    name="phone"
-                    value={personalInfo.phone}
-                    onChange={handlePersonalChange}
-                    placeholder="Nhập số điện thoại"
-                />
-
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={personalInfo.email}
-                    onChange={handlePersonalChange}
-                    placeholder="Nhập email"
-                />
-
-                <label htmlFor="address">Địa chỉ:</label>
-                <input
-                    type="text"
-                    id="address"
-                    name="address"
-                    value={personalInfo.address}
-                    onChange={handlePersonalChange}
-                    placeholder="Nhập địa chỉ"
-                />
-
-                <h3>Thông tin chuyên môn</h3>
-
-                <label htmlFor="qualification">Bằng cấp:</label>
-                <input
-                    type="text"
-                    id="qualification"
-                    name="qualification"
-                    value={professionalInfo.qualification}
-                    onChange={handleProfessionalChange}
-                    placeholder="Nhập bằng cấp"
-                />
-
-                <label htmlFor="experience">Kinh nghiệm:</label>
-                <input
-                    type="text"
-                    id="experience"
-                    name="experience"
-                    value={professionalInfo.experience}
-                    onChange={handleProfessionalChange}
-                    placeholder="Nhập kinh nghiệm"
-                />
-
-                <h3>Thông tin tài khoản</h3>
-
-                <label htmlFor="password">Mật khẩu:</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    value={accountInfo.password}
-                    onChange={handleAccountChange}
-                    placeholder="Nhập mật khẩu"
-                />
-
-                <label htmlFor="avatar">Chọn hình ảnh:</label>
-                <input
-                    type="file"
-                    id="avatar"
-                    name="avatar"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                />
-
-                {avatar && (
-                    <div>
-                        <p>Đã chọn ảnh: {avatar.name}</p>
-                        <img src={URL.createObjectURL(avatar)} alt="Avatar Preview" />
                     </div>
-                )}
 
-                <div className="action-buttons">
-                    <button type="submit">Xác nhận thông tin</button>
-                </div>
-            </form>
+                    <div className="mb-3">
+                        <label htmlFor="idCard" className="form-label">CMND/CCCD:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="idCard"
+                            name="idCard"
+                            value={personalInfo.idCard}
+                            onChange={handlePersonalChange}
+                            placeholder="Nhập CMND/CCCD"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="gender" className="form-label">Giới tính:</label>
+                        <div className="d-flex">
+                            <div className="form-check me-3">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="gender"
+                                    value="Nam"
+                                    checked={personalInfo.gender === 'Nam'}
+                                    onChange={handlePersonalChange}
+                                />
+                                <label className="form-check-label" htmlFor="nam">Nam</label>
+                            </div>
+                            <div className="form-check">
+                                <input
+                                    className="form-check-input"
+                                    type="radio"
+                                    name="gender"
+                                    value="Nữ"
+                                    checked={personalInfo.gender === 'Nữ'}
+                                    onChange={handlePersonalChange}
+                                />
+                                <label className="form-check-label" htmlFor="nu">Nữ</label>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="mb-3">
+                        <label htmlFor="dateOfBirth" className="form-label">Ngày sinh:</label>
+                        <input
+                            type="date"
+                            className="form-control"
+                            id="dateOfBirth"
+                            name="dateOfBirth"
+                            value={personalInfo.dateOfBirth}
+                            onChange={handlePersonalChange}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="phone" className="form-label">Số điện thoại:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="phone"
+                            name="phone"
+                            value={personalInfo.phone}
+                            onChange={handlePersonalChange}
+                            placeholder="Nhập số điện thoại"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email:</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={personalInfo.email}
+                            onChange={handlePersonalChange}
+                            placeholder="Nhập email"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="address" className="form-label">Địa chỉ:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="address"
+                            name="address"
+                            value={personalInfo.address}
+                            onChange={handlePersonalChange}
+                            placeholder="Nhập địa chỉ"
+                        />
+                    </div>
+
+                    <h3 className="mb-3">Thông tin chuyên môn</h3>
+
+                    <div className="mb-3">
+                        <label htmlFor="qualification" className="form-label">Bằng cấp:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="qualification"
+                            name="qualification"
+                            value={professionalInfo.qualification}
+                            onChange={handleProfessionalChange}
+                            placeholder="Nhập bằng cấp"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="experience" className="form-label">Kinh nghiệm:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="experience"
+                            name="experience"
+                            value={professionalInfo.experience}
+                            onChange={handleProfessionalChange}
+                            placeholder="Nhập kinh nghiệm"
+                        />
+                    </div>
+
+                    <h3 className="mb-3">Thông tin tài khoản</h3>
+
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Mật khẩu:</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={accountInfo.password}
+                            onChange={handleAccountChange}
+                            placeholder="Nhập mật khẩu"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="avatar" className="form-label">Chọn hình ảnh:</label>
+                        <input
+                            type="file"
+                            className="form-control"
+                            id="avatar"
+                            name="avatar"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                        />
+                    </div>
+
+                    {avatar && (
+                        <div>
+                            <p>Đã chọn ảnh: {avatar.name}</p>
+                            <img src={URL.createObjectURL(avatar)} alt="Avatar Preview" className="mb-3" />
+                        </div>
+                    )}
+
+                    <div className="action-buttons">
+                        <button type="submit" className="btn btn-primary">Xác nhận thông tin</button>
+                    </div>
+                </form>
+            </div>
         </div>
+
     );
 }
 
--- src/Home/header.js
+++ src/Home/header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import logo from '../assets/images/logo-01-01.png';
 import avatar from '../assets/images/avatar.jpg';
-import './header.css';
+import './header.scss';
 
 export default function Navigation() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -92,7 +92,7 @@ export default function Navigation() {
     //   </Modal>
     // </div>
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav id="header" class="navbar navbar-expand-lg bg-dark">
 
         <div class="container-fluid">
           <a className="nav-list--logo">
@@ -100,23 +100,21 @@ export default function Navigation() {
               <img src={logo} className="App-logo" alt="logo" />
             </Link>
           </a>
-          <a class="navbar-brand" href="#">Navbar</a>
+
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <Link class="nav-link text-light" to="/admin/doctorlist">List Doctor</Link>
               </li>
               <li class="nav-item">
-                <Link class="nav-link" to="/admin/doctorlist">List Doctor</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/admin/createslot">Create Slot</Link>
+                <Link class="nav-link text-light" to="/admin/createslot">Create Slot</Link>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
                 <ul class="dropdown-menu">
