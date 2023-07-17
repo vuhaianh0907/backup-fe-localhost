748a5719ea3c5f9f15c063203c6066d30a11bc5b
lucdq <lucdq410@gmail.com>
2023-07-18 00:13:33 +0700

    doctorupdateprofile

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -1,96 +1,54 @@
+// Footer.js
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaGoogle, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 import './Footer.scss';
 
-export default function Footer() {
-    return (
-        <footer className="text-center text-lg-start bg-light text-muted">
-            <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-                <div className="me-5 d-none d-lg-block">
-                    <span>Get connected with us on social networks:</span>
-                </div>
-                <div>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-google"></i>
-                    </a>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                    <a href="/" className="me-4 text-reset">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </div>
-            </section>
-            <section className="">
-                <div className="container text-center text-md-start mt-5">
-                    <div className="row mt-3">
-                        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                <i className="fas fa-gem me-3"></i>Company name
-                            </h6>
-                            <p>
-                                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit.
-                            </p>
-                        </div>
-                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                Products
-                            </h6>
-                            <p>
-                                <a href="/" className="text-reset">Angular</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">React</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">Vue</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">Laravel</a>
-                            </p>
-                        </div>
-                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                Useful links
-                            </h6>
-                            <p>
-                                <a href="/" className="text-reset">Pricing</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">Settings</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">Orders</a>
-                            </p>
-                            <p>
-                                <a href="/" className="text-reset">Help</a>
-                            </p>
-                        </div>
-                        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                            <p><i className="fas fa-home me-3"></i> Bình Định</p>
-                            <p>
-                                <i className="fas fa-envelope me-3"></i>
-                                lucdq@gmail.com
-                            </p>
-                            <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                            <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-                © 2021 Company. All rights reserved.
-            </div>
-        </footer>
-    );
-}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <section className="footer-content">
+        <div className="footer-column">
+          <h3>Company name</h3>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+          <div className="social-icons">
+            <a href="/" className="social-icon"><FaFacebookF /></a>
+            <a href="/" className="social-icon"><FaTwitter /></a>
+            <a href="/" className="social-icon"><FaGoogle /></a>
+            <a href="/" className="social-icon"><FaInstagram /></a>
+            <a href="/" className="social-icon"><FaLinkedin /></a>
+            <a href="/" className="social-icon"><FaGithub /></a>
+          </div>
+        </div>
+        <div className="footer-column">
+          <h3>Products</h3>
+          <p><a href="/" className="text-reset">Angular</a></p>
+          <p><a href="/" className="text-reset">React</a></p>
+          <p><a href="/" className="text-reset">Vue</a></p>
+          <p><a href="/" className="text-reset">Laravel</a></p>
+        </div>
+        <div className="footer-column">
+          <h3>Useful links</h3>
+          <p><a href="/" className="text-reset">Pricing</a></p>
+          <p><a href="/" className="text-reset">Settings</a></p>
+          <p><a href="/" className="text-reset">Orders</a></p>
+          <p><a href="/" className="text-reset">Help</a></p>
+        </div>
+        <div className="footer-column">
+          <h3>Contact</h3>
+          <p><i className="fas fa-home me-3"></i> Bình Định</p>
+          <p><i className="fas fa-envelope me-3"></i> lucdq@gmail.com</p>
+          <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+      </section>
+      <div className="footer-bottom">
+        <p>© {new Date().getFullYear()} Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
