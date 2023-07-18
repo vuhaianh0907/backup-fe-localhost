9863a5017139aab47a160d27803b5ad915850267
vuhaianh0907 <vuhaianh0907@gmail.com>
2023-07-18 09:07:54 +0700

    pull

--- src/BodyContent/accout/ForgotPassword/ForgotPassword.js
+++ src/BodyContent/accout/ForgotPassword/ForgotPassword.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ForgotPassword.scss';
 import axios from 'axios';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Thực hiện xử lý gửi email đặt lại mật khẩu với địa chỉ email đã nhập
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div id="ForgotPassword" className="forgot-password-container">
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/forgetpassword', { email });
+      if (response.status === 200) {
+        alert('Vui lòng kiểm tra email để đặt lại mật khẩu.');
+      } else {
+        alert('Gửi yêu cầu đặt lại mật khẩu thất bại.');
+      }
+    } catch (error) {
+      console.error('Error sending reset password request:', error);
+      alert('Gửi yêu cầu đặt lại mật khẩu thất bại.');
+    }
+  };
 
-            <form onSubmit={handleSubmit}>
-                <h2>Quên mật khẩu</h2>
-                <input type="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required /><br />
-                <button type="submit" name="action" value="reset">Đặt lại mật khẩu</button>
-                <div className='back-to-login'>
-                    <a href="/login" className='register-link'>Quay lại đăng nhập</a>
-                </div>
-            </form>
-
+  return (
+    <div id="ForgotPassword" className="forgot-password-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Quên mật khẩu</h2>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br />
+        <button type="submit" name="action" value="reset">
+          Đặt lại mật khẩu
+        </button>
+        <div className="back-to-login">
+          <a href="/login" className="register-link">
+            Quay lại đăng nhập
+          </a>
         </div>
-
-
-    );
+      </form>
+    </div>
+  );
 }
