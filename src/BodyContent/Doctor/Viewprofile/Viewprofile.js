175600351140d07440cfcd08c3dbbe37fcbaa228
lucdq <lucdq410@gmail.com>
2023-07-31 03:11:42 +0700

    loading v8

--- src/BodyContent/Doctor/Viewprofile/Viewprofile.js
+++ src/BodyContent/Doctor/Viewprofile/Viewprofile.js
@@ -117,87 +117,89 @@ const DoctorViewProfile = () => {
 
   return (
     <div id="doctorViewProfile" className="doctor-detail">
-      <div className="avatar-container">
-        <img src="https://via.placeholder.com/150" alt="Doctor Avatar" className="doctor-avatar" />
-      </div>
-      <div className="info-container">
-        <div className="doctor-name">{doctorInfo.fullname}</div>
-        <div className="doctor-info">
-          <span className="info-label">ID Card:</span>
-          <span>{doctorInfo.idCard}</span>
+      <div className='doctorbrc'>
+        <div className="avatar-container">
+          <img src="https://via.placeholder.com/150" alt="Doctor Avatar" className="doctor-avatar" />
         </div>
-        <div className="doctor-info">
-          <span className="info-label">Gender:</span>
-          <span>{doctorInfo.gender}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Date of Birth:</span>
-          <span>{doctorInfo.dateOfBirth}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Phone Number:</span>
-          <span>{doctorInfo.phone}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Email:</span>
-          <span>{doctorInfo.email}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Address:</span>
-          <span>{doctorInfo.address}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Qualification:</span>
-          <span>{doctorInfo.qualification}</span>
-        </div>
-        <div className="doctor-info">
-          <span className="info-label">Experience:</span>
-          <span>{doctorInfo.experience}</span>
-        </div>
-        <div className="filter-container">
-          <label htmlFor="dateFilter">Select Date:</label>
-          <input type="date" id="dateFilter" value={selectedDate} onChange={handleDateChange} />
-        </div>
+        <div className="info-container">
+          <div className="doctor-name">{doctorInfo.fullname}</div>
+          <div className="doctor-info">
+            <span className="info-label">ID Card:</span>
+            <span>{doctorInfo.idCard}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Gender:</span>
+            <span>{doctorInfo.gender}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Date of Birth:</span>
+            <span>{doctorInfo.dateOfBirth}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Phone Number:</span>
+            <span>{doctorInfo.phone}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Email:</span>
+            <span>{doctorInfo.email}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Address:</span>
+            <span>{doctorInfo.address}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Qualification:</span>
+            <span>{doctorInfo.qualification}</span>
+          </div>
+          <div className="doctor-info">
+            <span className="info-label">Experience:</span>
+            <span>{doctorInfo.experience}</span>
+          </div>
+          <div className="filter-container">
+            <label htmlFor="dateFilter">Select Date:</label>
+            <input type="date" id="dateFilter" value={selectedDate} onChange={handleDateChange} />
+          </div>
 
-        <div className="slots-container">
-          <h3>Slots:</h3>
-          {filteredSlots.length > 0 ? (
-            filteredSlots.map((slot) => (
-              <button
-                key={slot.id}
-                className={`slot-button ${slot.status === 'available' ? 'green' : 'gray'}`}
-                onClick={() => handleSlotClick(slot)}
-              >
-                {slot.time}
-              </button>
-            ))
-          ) : (
-            <div>No slots available</div>
-          )}
-        </div>
+          <div className="slots-container">
+            <h3>Slots:</h3>
+            {filteredSlots.length > 0 ? (
+              filteredSlots.map((slot) => (
+                <button
+                  key={slot.id}
+                  className={`slot-button ${slot.status === 'available' ? 'green' : 'gray'}`}
+                  onClick={() => handleSlotClick(slot)}
+                >
+                  {slot.time}
+                </button>
+              ))
+            ) : (
+              <div>No slots available</div>
+            )}
+          </div>
 
-        <div className="options-button-container">
-          <button className="options-button" onClick={handleUpdateProfileClick}>
-            Chỉnh sửa
-          </button>
-        </div>
-      </div>
-
-      {showPopup && (
-        <div className="popup">
-          <div className="popup-content">
-            <h3>Bạn có muốn đóng slot này không?</h3>
-            <div className="popup-buttons">
-              <button className="popup-button" onClick={handlePopupClose}>
-                Đóng
-              </button>
-              <button className="popup-button" onClick={handleConfirmClick}>
-                Xác nhận
-              </button>
-            </div>
+          <div className="options-button-container">
+            <button className="options-button" onClick={handleUpdateProfileClick}>
+              Chỉnh sửa
+            </button>
           </div>
         </div>
-      )}
+
+        {showPopup && (
+          <div className="popup">
+            <div className="popup-content">
+              <h3>Bạn có muốn đóng slot này không?</h3>
+              <div className="popup-buttons">
+                <button className="popup-button" onClick={handlePopupClose}>
+                  Đóng
+                </button>
+                <button className="popup-button" onClick={handleConfirmClick}>
+                  Xác nhận
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
